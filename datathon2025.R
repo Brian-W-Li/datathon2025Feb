7dfd4f6b3da2c079eb2de2e3933f90e546aefdae
Brian-W-Li <brian.li.fml@gmail.com>
2025-02-22 14:33:07 -0800

    added team names

--- datathon2025.R
+++ datathon2025.R
@@ -8,23 +8,16 @@
 #library(RSQLite)
 
 conn <- dbConnect(RSQLite::SQLite(), "my_database.sqlite")
-LEAGUE_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-LEAGUE.csv", 
+ATTRIBUTES_data <- read.csv("/Users/Brian/Downloads/datathon2025_group21/european_soccer_dataset-PLAYER_ATTRIBUTES.csv", 
                         stringsAsFactors = FALSE)
-PLAYER_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-PLAYER.csv", stringsAsFactors = FALSE)
-PLAYER_ATTRIBUTES_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-PLAYER_ATTRIBUTES.csv", stringsAsFactors = FALSE)
-TEAM_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-TEAM.csv", stringsAsFactors = FALSE)
-MATCH_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-MATCH.csv", stringsAsFactors = FALSE)
-COUNTRY_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-COUNTRY.csv", stringsAsFactors = FALSE)
-TEAM_ATTRIBUTES_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-TEAM_ATTRIBUTES.csv", stringsAsFactors = FALSE)
+MATCH_data <- read.csv("/Users/Brian/Downloads/datathon2025_group21/european_soccer_dataset-MATCH.csv", stringsAsFactors = FALSE)
+TEAM_data <- read.csv("/Users/Brian/Downloads/datathon2025_group21/european_soccer_dataset-TEAM.csv", stringsAsFactors = FALSE)
+
 #head(LEAGUE_data)
 # Write each data frame to the SQLite database
-dbWriteTable(conn, "LEAGUE", LEAGUE_data, overwrite = TRUE, row.names = FALSE)
-dbWriteTable(conn, "PLAYER", PLAYER_data, overwrite = TRUE, row.names = FALSE)
 dbWriteTable(conn, "PLAYER_ATTRIBUTES", PLAYER_ATTRIBUTES_data, overwrite = TRUE, row.names = FALSE)
 dbWriteTable(conn, "TEAM", TEAM_data, overwrite = TRUE, row.names = FALSE)
 dbWriteTable(conn, "MATCH", MATCH_data, overwrite = TRUE, row.names = FALSE)
-dbWriteTable(conn, "COUNTRY", COUNTRY_data, overwrite = TRUE, row.names = FALSE)
-dbWriteTable(conn, "TEAM_ATTRIBUTES", TEAM_ATTRIBUTES_data, overwrite = TRUE, row.names = FALSE)
 dbListTables(conn)
 head(MATCH_data)
 
