0c6c02804ae7b81ef31638b7a217fb370a5e7790
Brian-W-Li <brian.li.fml@gmail.com>
2025-02-22 12:09:45 -0800

    aggression

--- datathon2025.R
+++ datathon2025.R
@@ -7,7 +7,7 @@
 #library(DBI)
 #library(RSQLite)
 
-#conn <- dbConnect(RSQLite::SQLite(), "my_database.sqlite")
+conn <- dbConnect(RSQLite::SQLite(), "my_database.sqlite")
 LEAGUE_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-LEAGUE.csv", 
                         stringsAsFactors = FALSE)
 PLAYER_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-PLAYER.csv", stringsAsFactors = FALSE)
@@ -17,8 +17,27 @@ MATCH_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanS
 COUNTRY_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-COUNTRY.csv", stringsAsFactors = FALSE)
 TEAM_ATTRIBUTES_data <- read.csv("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-TEAM_ATTRIBUTES.csv", stringsAsFactors = FALSE)
 #head(LEAGUE_data)
-ls()
+# Write each data frame to the SQLite database
+dbWriteTable(conn, "LEAGUE", LEAGUE_data, overwrite = TRUE, row.names = FALSE)
+dbWriteTable(conn, "PLAYER", PLAYER_data, overwrite = TRUE, row.names = FALSE)
+dbWriteTable(conn, "PLAYER_ATTRIBUTES", PLAYER_ATTRIBUTES_data, overwrite = TRUE, row.names = FALSE)
+dbWriteTable(conn, "TEAM", TEAM_data, overwrite = TRUE, row.names = FALSE)
+dbWriteTable(conn, "MATCH", MATCH_data, overwrite = TRUE, row.names = FALSE)
+dbWriteTable(conn, "COUNTRY", COUNTRY_data, overwrite = TRUE, row.names = FALSE)
+dbWriteTable(conn, "TEAM_ATTRIBUTES", TEAM_ATTRIBUTES_data, overwrite = TRUE, row.names = FALSE)
+dbListTables(conn)
+
+query <- "SELECT p.player_name, pa.aggression
+          FROM PLAYER_ATTRIBUTES pa
+          JOIN PLAYER p ON p.player_api_id = pa.player_api_id
+          ORDER BY pa.aggression DESC
+          LIMIT 11;"
+
+aggressive_players <- dbGetQuery(conn, query)
+print(aggressive_players)
+
+
+
 #head(my_data)
 #dbWriteTable(conn, "my_table", my_data, overwrite = TRUE, row.names = FALSE)
 
-#file.exists("/Users/Brian/Downloads/PredictiveModelingTrack-EuropeanSoccerDataset/european_soccer_dataset-LEAGUE.csv")
