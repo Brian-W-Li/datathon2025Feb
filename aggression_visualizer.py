e7aea2fa6aa4473af26095950f0f007e88e92ec1
Sir-Edward-123 <rzhu.mathleague@gmail.com>
2025-02-22 17:56:06 -0800

    Update aggression_visualizer
    Getting matches of interest for home and away games

--- aggression_visualizer.py
+++ aggression_visualizer.py
@@ -3,6 +3,51 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-match_data = pd.read_csv('european_soccer_dataset-MATCH.csv')
-player_attribute_data = pd.read_csv('european_soccer_dataset-PLAYER_ATTRIBUTES.csv')
+# Load csv file
+match_aggr_data = pd.read_csv("soccer_match_aggression.csv")
+
+# Get team to analyze
+# Can convert team name to team selection id later
+team_selection_id = input("Enter Team ID: ")
+
+
+
+# Get columns where the team inputted is the home team
+match_api_id = match_aggr_data.loc[
+    match_aggr_data["home_team_api_id"] == team_selection_id,
+    "match_api_id"]
+team_goal = match_aggr_data.loc[
+    match_aggr_data["home_team_api_id"] == team_selection_id,
+    "home_team_goal"]
+team_aggression = match_aggr_data.loc[
+    match_aggr_data["home_team_api_id"] == team_selection_id,
+    "home_team_aggression"]
+other_team_goal = match_aggr_data.loc[
+    match_aggr_data["home_team_api_id"] == team_selection_id,
+    "away_team_goal"]
+other_team_aggression = match_aggr_data.loc[
+    match_aggr_data["home_team_api_id"] == team_selection_id,
+    "away_team_aggression"]
+
+
+
+# Get columns where the team inputted is the away team
+match_api_id = match_aggr_data.loc[
+    match_aggr_data["away_team_api_id"] == team_selection_id,
+    "match_api_id"]
+team_goal = match_aggr_data.loc[
+    match_aggr_data["away_team_api_id"] == team_selection_id,
+    "away_team_goal"]
+team_aggression = match_aggr_data.loc[
+    match_aggr_data["away_team_api_id"] == team_selection_id,
+    "away_team_aggression"]
+other_team_goal = match_aggr_data.loc[
+    match_aggr_data["away_team_api_id"] == team_selection_id,
+    "home_team_goal"]
+other_team_aggression = match_aggr_data.loc[
+    match_aggr_data["away_team_api_id"] == team_selection_id,
+    "home_team_aggression"]
+
+
+
 
